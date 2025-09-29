d54149530d4c8a320fd548c2d51a78d1b70d4220
Emilio Bensignor <liobensignor@gmail.com>
2025-09-29 11:44:23 -0300

    Sacar comentarios

--- componentes/registrar_cliente.php
+++ componentes/registrar_cliente.php
@@ -3,10 +3,8 @@ $username = $_POST['username'];
 $email = $_POST['email'];
 $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
-// Conectar con la DB
 include 'conexion.php';
 
-// Enviar datos a la tabla
 mysqli_query($datosDB, "INSERT INTO usuarios VALUES ('$username', '$email', '$password')");
 
 header("Location: ../login.php?ok_registro");
