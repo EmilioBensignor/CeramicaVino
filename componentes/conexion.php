e3686710617a05bc097c7a0b39bf8c1581f390f3
Emilio Bensignor <liobensignor@gmail.com>
2025-11-03 12:52:08 -0300

    Momento 3

--- componentes/conexion.php
+++ componentes/conexion.php
@@ -1,7 +1,10 @@
 <?php
 // Conexion a XAMPP
-$datosDB = mysqli_connect("localhost", "root", "", "ceramica_vino") or exit ("Error al conectar a la base de datos");
+// $datosDB = mysqli_connect("localhost", "root", "", "ceramica_vino") or exit ("Error al conectar a la base de datos");
 
 // Conexion a InfinityFree
-// $datosDB = mysqli_connect("sql203.infinityfree.com", "if0_39630596", "XrrSWnQ6Dy1", "if0_39630596_ceramica_vino") or exit ("Error al conectar a la base de datos");
+$datosDB = mysqli_connect("sql203.infinityfree.com", "if0_39630596", "XrrSWnQ6Dy1", "if0_39630596_ceramica_vino") or exit ("Error al conectar a la base de datos");
+
+// Establecer charset UTF-8 para caracteres especiales
+mysqli_set_charset($datosDB, "utf8mb4");
 ?>
